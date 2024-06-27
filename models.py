2320498201fda65e9d09b7dfab27f891e40068bb
vikash kumar singh <drv74279@gmail.com>
2024-06-27 16:32:09 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,20 @@
+# models.py
+ 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+ 
+Base = declarative_base()
+ 
+class Application(Base):
+    __tablename__ = "applications"
+    id = Column(Integer, primary_key=True, index=True)
+    fullName = Column(String, index=True)
+    phone = Column(String, index=True)
+    email = Column(String, index=True)
+    resumePath = Column(String)  # Store full path to resume file
+    photoPath = Column(String, nullable=True)  # Store full path to photo file if uploaded
+    address = Column(String)
+    position = Column(String)
+    gender = Column(String)
+    qualification = Column(String)
+    reference = Column(String, nullable=True)
